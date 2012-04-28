efd3c869f54648621cba7feea16cffe634750a64
Muccy <muccymac@gmail.com>
2012-04-28 14:31:14 +0200

    First Commit

---
+++ MUKMediaGallery/Unit Tests/MUKMediaGalleryTests.h
@@ -0,0 +1,13 @@
+//
+//  MUKMediaGalleryTests.h
+//  MUKMediaGalleryTests
+//
+//  Created by Marco Muccinelli on 28/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MUKMediaGalleryTests : SenTestCase
+
+@end
